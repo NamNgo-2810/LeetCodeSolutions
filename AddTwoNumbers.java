b1a7e7001d57100639f416b559dd59168e762e0d
NamNgo-2810 <59565159+NamNgo-2810@users.noreply.github.com>
2020-09-26 21:32:15 +0700

    Update AddTwoNumbers.java

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,3 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class AddTwoNumbers {
     // Problem 2: Add Two Numbers
     public ListNode addTwoNumbers(ListNode l1, ListNode l2, int c) {
